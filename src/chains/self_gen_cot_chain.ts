3dce73b60d1747500c0c99a5dafd770f865fdfa0
Bell Eapen <github_public@gulfdoctor.net>
2023-12-31 12:16:36 +0000

    Refactor SelfGenCotChain class to use private variables

--- src/chains/self_gen_cot_chain.ts
+++ src/chains/self_gen_cot_chain.ts
@@ -7,13 +7,9 @@ import { LLM } from "langchain/llms/base";
 
 export class SelfGenCotChain {
 
-    constructor() {
-
-    }
-
-    llm: LLM = container.resolve("main-llm");
-    name: string = "self generated explanation";
-    template: string = `
+    private _llm: LLM = container.resolve("main-llm");
+    private _name: string = "self generated explanation";
+    private _template: string = `
         ## Question: {question}
         ## Answer: {answer}
         Given the above question and answer, generate a chain of thought explanation for the answer.
@@ -22,13 +18,26 @@ export class SelfGenCotChain {
         Therefore, the answer is {answer}.
         `;
 
+    constructor(
+        llm?: LLM,
+        name?: string,
+        template?: string,
+    ) {
+        if (llm)
+            this._llm = llm;
+        if (name)
+            this._name = name;
+        if (template)
+            this._template = template;
+    }
+
     promptTemplate: PromptTemplate = new PromptTemplate({
-        template: this.template,
+        template: this._template,
         inputVariables: ["question", "answer"],
     });
 
     chain: LLMChain = new LLMChain({
-        llm: this.llm,
+        llm: this._llm,
         prompt: this.promptTemplate,
         outputKey: "explanation",
     });
