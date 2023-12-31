f2e772daa21f45b165a27645669ed132ae52e337
Bell Eapen <github_public@gulfdoctor.net>
2023-12-31 12:28:04 +0000

    Refactor SelfGenCotChain class properties and add getters

--- src/chains/self_gen_cot_chain.ts
+++ src/chains/self_gen_cot_chain.ts
@@ -1,15 +1,30 @@
 import "../bootstrap";
-import { SequentialChain, LLMChain } from "langchain/chains";
+import { SequentialChain, LLMChain, BaseChain } from "langchain/chains";
 import { container } from "tsyringe";
 import { PromptTemplate } from "langchain/prompts";
 import { LLM } from "langchain/llms/base";
+import { BaseCache, ChainValues } from "langchain/dist/schema";
+import { CallbackManagerForChainRun } from "langchain/dist/callbacks";
 
 
 export class SelfGenCotChain {
+    _call(values: ChainValues, runManager?: CallbackManagerForChainRun | undefined): Promise<ChainValues> {
+        throw new Error("Method not implemented.");
+    }
+    _chainType(): string {
+        throw new Error("Method not implemented.");
+    }
+    get inputKeys(): string[] {
+        throw new Error("Method not implemented.");
+    }
+    get outputKeys(): string[] {
+        throw new Error("Method not implemented.");
+    }
 
-    private _llm: LLM = container.resolve("main-llm");
-    private _name: string = "self generated explanation";
-    private _template: string = `
+    private llm: LLM = container.resolve("main-llm");
+    name: string = "self generated explanation";
+    description: string = "Generate a chain of thought explanation for the answer to a question.";
+    private template: string = `
         ## Question: {question}
         ## Answer: {answer}
         Given the above question and answer, generate a chain of thought explanation for the answer.
@@ -22,22 +37,25 @@ export class SelfGenCotChain {
         llm?: LLM,
         name?: string,
         template?: string,
+        description?: string,
     ) {
         if (llm)
-            this._llm = llm;
+            this.llm = llm;
         if (name)
-            this._name = name;
+            this.name = name;
         if (template)
-            this._template = template;
+            this.template = template;
+        if (description)
+            this.description = description;
     }
 
     promptTemplate: PromptTemplate = new PromptTemplate({
-        template: this._template,
+        template: this.template,
         inputVariables: ["question", "answer"],
     });
 
     chain: LLMChain = new LLMChain({
-        llm: this._llm,
+        llm: this.llm,
         prompt: this.promptTemplate,
         outputKey: "explanation",
     });
@@ -45,6 +63,18 @@ export class SelfGenCotChain {
     async call(args: { question: string, answer: string }) {
         const chainExecutionResult = await this.chain.call(args);
         return chainExecutionResult;
+    }
+
+    public getName() {
+        return this.name;
+    }
+
+    public getTemplate() {
+        return this.template;
+    }
+
+    public getDescription() {
+        return this.description;
     }
 
 }
