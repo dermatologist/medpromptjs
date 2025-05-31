e90d16ceb3c59804cfa8ac073ef8693d358f6de7
dermatologist <github@gulfdoctor.net>
2025-05-31 18:22:06 -0500

    test: add unit tests for LLMLoop getters and setters

--- test/llm_loop.test.ts
+++ test/llm_loop.test.ts
@@ -16,6 +16,23 @@ describe('LLMLoop', () => {
     jest.clearAllMocks();
   });
 
+
+  // Add tests for getters and setters
+  it('should set and get mapQueryTemplate', () => {
+    llmLoop.mapQueryTemplate = 'test-template';
+    expect(llmLoop.mapQueryTemplate).toBe('test-template');
+  });
+
+  it('should set and get mapDocTemplate', () => {
+    llmLoop.mapDocTemplate = 'test-doc-template';
+    expect(llmLoop.mapDocTemplate).toBe('test-doc-template');
+  });
+
+  it('should set and get reduceChainTemplate', () => {
+    llmLoop.reduceChainTemplate = 'test-reduce-template';
+    expect(llmLoop.reduceChainTemplate).toBe('test-reduce-template');
+  });
+
   it('should initialize with default values', () => {
     expect(llmLoop.name).toBe('lLMLoop');
     expect(llmLoop.description).toBe('l_l_m_loop');
